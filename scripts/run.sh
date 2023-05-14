07bc8a6bba8c444af07fc1aa111c6ed4649ce84a
Rafał Cisek <rafal.cisek121@gmail.com>
2023-05-14 00:16:32 +0000

    Fix for dlload steamclient.so

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,8 @@
 sh download_server_files.sh
 cp start_server.sh server_files
 
+mkdir -p /root/.steam/sdk64/
+cp steamcmd/linux64/steamclient.so /root/.steam/sdk64
+
 cd server_files
 sh start_server.sh
